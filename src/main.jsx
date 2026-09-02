9ac637bc951b3a3e3f4cd15da4ab739b107dd0aa
nicolasdalmont <nicolas.dalmont@laposte.net>
2026-09-02 19:02:11 +0200

    Initialize auto-update in main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { initAutoUpdate } from './versionCheck.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+initAutoUpdate();
